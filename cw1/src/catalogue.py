b3c23b4bc6e60ce16079cb9de4b829ea1838788f
40183396 <40183396@live.napier.ac.uk>
2016-10-17 15:02:31 +0000

    Added more items to table

--- cw1/src/catalogue.py
+++ cw1/src/catalogue.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    dict = {'East Of Eden':'John Steinbeck'}
+    dict = {'East Of Eden':'John Steinbeck',
+    'For Whom The Bell Tolls':'Ernest Hemingway',
+    'Catch-22':'Joseph Heller'}
     return render_template('catalogue.html', root = dict)
 
 if __name__ == "__main__":
