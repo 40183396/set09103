515c472f70cfe38ce9dcec6e8c0b28ed27fa9489
40183396 <40183396@live.napier.ac.uk>
2016-10-17 14:27:31 +0000

    Trying to add pictures to table

--- cw1/src/catalogue.py
+++ cw1/src/catalogue.py
@@ -1,9 +1,10 @@
-from flask import Flask
-epp = Flask(__name__)
+from flask import Flask, redirect, url_for, abort, request, render_template
+app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def root():
+    dict = {'East Of Eden':'John Steinbeck'}
+    return render_template('catalogue.html', root = dict)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
